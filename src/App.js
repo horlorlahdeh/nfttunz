4c4996ea375d9bb45cb528e433acb804f3d0ee88
horlorlahdeh <horlorlahdeh@gmail.com>
2021-07-17 22:57:57 +0100

    second init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello from App</h1>
+      <Home />
     </div>
   );
 }
