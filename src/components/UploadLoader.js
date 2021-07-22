18d0425bb8b58a9b96e96a633d50853621f44e3b
horlorlahdeh <horlorlahdeh@gmail.com>
2021-07-22 16:40:23 +0100

    upload loader

---
+++ src/components/UploadLoader.js
@@ -0,0 +1,7 @@
+import React from "react";
+import upload_loader from "../assets/images/upload_loader_trans.gif";
+const UploadLoader = () => {
+  return <img src={upload_loader} alt="loading..." width={40} />;
+};
+
+export default UploadLoader;
