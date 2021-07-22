b72447d6c8f0c37ba0b33690d0a54dcfb20e8ade
horlorlahdeh <horlorlahdeh@gmail.com>
2021-07-22 16:40:58 +0100

    upload file helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,41 @@
 import { toast } from "react-toastify";
+import axios from "./axios";
 
-export const setToastNotification =  async (message, type) => {
-  toast(message, {type});
-}
+export const setToastNotification = async (message, type) => {
+  toast(message, { type });
+};
 
 export const toFixedWithoutRounding = (t, l = 3) => {
-  const a = 10 ** l
-  const s = t * a
-  return Math.trunc(s) / a
-}
+  const a = 10 ** l;
+  const s = t * a;
+  return Math.trunc(s) / a;
+};
+
+export const uploadFile = async (file, type) => {
+  try {
+    var percentage;
+    const config = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress: function (progressEvent) {
+        var percentCompleted = parseInt(
+          Math.round((progressEvent.loaded * 100) / progressEvent.total)
+        );
+        percentage = percentCompleted;
+      },
+    };
+    const data_upload = new FormData();
+    data_upload.append("file", file);
+    data_upload.append("type", type);
+    const data = await axios.post("/upload", data_upload, config);
+    const obj = {
+      data: data.data,
+      progress: percentage,
+    };
+    return obj;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
