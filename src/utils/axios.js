b539bd893d1cc1d30f7947a095b0ca8e407fbb9a
horlorlahdeh <horlorlahdeh@gmail.com>
2021-08-22 20:43:01 +0100

    http

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 /** base url to make request to the BE end point */
 
 const instance = axios.create({
-	baseURL: 'api.nfttunz.io/'
+	baseURL: 'http://api.nfttunz.io/'
 });
 
 export default instance;
